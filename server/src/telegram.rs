187d55f16a2923a80e2328c5043c818bbf503134
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-11-30 13:43:54 +0700

    Rewrite telegram bot with frankenstein

--- server/src/telegram.rs
+++ server/src/telegram.rs
@@ -1,14 +1,21 @@
 use crate::{config::TelegramConfig, recepient::Recepient, statistics::ChannelHistory};
+use frankenstein::{
+    AllowedUpdate, AsyncApi, AsyncTelegramApi, GetUpdatesParams, Message, ParseMode,
+    SendMessageParams, UpdateContent,
+};
 use rtherm_common::{ChannelId, Measurements};
 use serde::{Deserialize, Serialize};
 use std::{
+    borrow::Cow,
     collections::{hash_map::Entry, HashMap},
     ops::RangeInclusive,
+    str::FromStr,
     sync::Arc,
     time::{Duration, Instant},
 };
-use teloxide::{prelude::*, types::ChatId, utils::command::BotCommands, RequestError};
 use tokio::{sync::RwLock, task::spawn, time::sleep};
+
+type ChatId = i64;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct CommonSettings {
@@ -98,94 +105,301 @@ struct State {
 
 type SharedState = Arc<RwLock<State>>;
 
-impl State {
+impl ChannelState {
     fn digest(&self) -> String {
-        let mut text = String::new();
-        for (id, channel) in self.channels.iter() {
-            let stats = channel.values.statistics();
-            text = format!("{}\n`{}`:\n{}", text, id, stats);
+        self.values.statistics().to_string()
+    }
+    fn last_value_text(&self) -> String {
+        if let Some(point) = self.values.statistics().last {
+            return format!("{:.1} °C", point.value);
         }
-        text
+        return "offline".into();
     }
 }
 
-#[derive(BotCommands, Clone)]
-#[command(
-    rename_rule = "lowercase",
-    description = "These commands are supported:"
-)]
+impl State {
+    fn digest(&self) -> String {
+        if !self.channels.is_empty() {
+            self.channels
+                .iter()
+                .map(|(id, channel)| format!("/digest_{id}: {}\n", channel.last_value_text()))
+                .collect()
+        } else {
+            "No active channels".to_string()
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 enum Command {
-    #[command(description = "display this text.")]
     Help,
-    #[command(description = "show all available channels.")]
-    Channels,
-    #[command(description = "subscribe to a specific channel.")]
-    Subscribe { channel: ChannelId },
+    Digest { channel: Option<ChannelId> },
+    Subscribe { channel: Option<ChannelId> },
+    Unsubscribe { channel: Option<ChannelId> },
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Command, state: SharedState) -> ResponseResult<()> {
-    match cmd {
-        Command::Help => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
-        }
-        Command::Channels => {
-            let state = state.read().await;
-            bot.send_message(msg.chat.id, state.digest()).await?
-        }
-        Command::Subscribe { channel } => {
-            if channel.is_empty() {
-                bot.send_message(msg.chat.id, "Please provide channel name")
-                    .await?
-            } else {
-                let mut state = state.write().await;
-                let chat = state.chats.entry(msg.chat.id).or_default();
-                let entry = chat.subscriptions.entry(channel.clone());
-                let subscribed = matches!(&entry, Entry::Vacant(..));
-                entry.or_default();
-                bot.send_message(
-                    msg.chat.id,
-                    format!(
-                        "You {} to channel `{}`.",
-                        if subscribed {
-                            "have successfully subscribed"
-                        } else {
-                            "are already subscribed"
-                        },
-                        channel
-                    ),
-                )
-                .await?
-            }
-        }
-    };
-
-    Ok(())
+impl Command {
+    const HELP: &'static str = r#"Available commands:
+/help - display this text.
+/digest - show info about all channels or a selected one.
+/subscribe - subscribe to a specified channel.
+/unsubscribe - unsubscribe from a previously subscribed channel.
+"#;
 }
 
+impl FromStr for Command {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = if s.contains('_') {
+            Cow::Owned(s.replace('_', " "))
+        } else {
+            Cow::Borrowed(s)
+        };
+        let mut args = s.split_whitespace();
+        let cmd = args.next().ok_or("Empty command")?;
+        if !cmd.starts_with('/') {
+            return Err("Command must start with '/'".into());
+        }
+        let ret = match &cmd[1..] {
+            "start" | "help" => Self::Help,
+            "digest" => Self::Digest {
+                channel: args.next().map(String::from),
+            },
+            "subscribe" => Self::Subscribe {
+                channel: args.next().map(String::from),
+            },
+            "unsubscribe" => Self::Unsubscribe {
+                channel: args.next().map(String::from),
+            },
+            other => return Err(format!("Unknown command: {other}")),
+        };
+        if let Some(extra) = args.next() {
+            return Err(format!("Unexpected argument: {extra}"));
+        }
+        Ok(ret)
+    }
+}
+
+#[derive(Clone)]
 pub struct Telegram {
-    bot: Bot,
+    api: AsyncApi,
     state: SharedState,
+}
+
+type Error = <AsyncApi as AsyncTelegramApi>::Error;
+
+async fn send_message(api: &AsyncApi, chat: ChatId, text: impl Into<String>) -> Result<(), Error> {
+    api.send_message(
+        &SendMessageParams::builder()
+            .parse_mode(ParseMode::Html)
+            .chat_id(chat)
+            .text(text)
+            .build(),
+    )
+    .await?;
+    Ok(())
 }
 
 impl Telegram {
     pub async fn new(config: TelegramConfig) -> Self {
-        let state = SharedState::default();
-        let bot = Bot::new(config.token);
-        spawn(monitor(bot.clone(), state.clone()));
-        spawn(Command::repl(bot.clone(), {
-            let state = state.clone();
-            move |bot, msg, cmd| answer(bot, msg, cmd, state.clone())
-        }));
-        Self { bot, state }
+        let this = Self {
+            api: AsyncApi::new(&config.token),
+            state: SharedState::default(),
+        };
+        spawn(this.clone().poll());
+        spawn(this.clone().monitor());
+        this
+    }
+
+    async fn process_message(&self, msg: Message) -> Result<(), Error> {
+        let chat = msg.chat.id;
+        if let Some(text) = msg.text {
+            match Command::from_str(&text) {
+                Ok(cmd) => self.process_command(chat, cmd).await?,
+                Err(reason) => send_message(&self.api, chat, format!("Error: {reason}")).await?,
+            }
+        } else {
+            send_message(&self.api, chat, "Error: Only text commands are supported").await?;
+        };
+        Ok(())
+    }
+
+    async fn poll(self) -> ! {
+        let mut params = GetUpdatesParams::builder()
+            .allowed_updates(vec![AllowedUpdate::Message])
+            .build();
+        loop {
+            let update = match self.api.get_updates(&params).await {
+                Ok(u) => u,
+                Err(err) => {
+                    log::error!("Cannot get updates: {err}");
+                    continue;
+                }
+            };
+            for update in update.result {
+                params.offset = Some(update.update_id as i64 + 1);
+                match update.content {
+                    UpdateContent::Message(msg) => {
+                        if let Err(err) = self.process_message(msg).await {
+                            log::error!("Error processing message: {err}");
+                        }
+                    }
+                    _ => {
+                        log::error!("Unexpected content type");
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    async fn process_command(&self, chat_id: ChatId, cmd: Command) -> Result<(), Error> {
+        match cmd {
+            Command::Help => send_message(&self.api, chat_id, Command::HELP).await?,
+            Command::Digest { channel } => {
+                let state = self.state.read().await;
+                send_message(
+                    &self.api,
+                    chat_id,
+                    if let Some(id) = channel {
+                        match state.channels.get(&id) {
+                            Some(chan) => chan.digest(),
+                            None => format!("Error: No such channel <code>{id}</code>"),
+                        }
+                    } else {
+                        state.digest()
+                    },
+                )
+                .await?;
+            }
+            Command::Subscribe { channel } => {
+                if let Some(channel) = channel {
+                    let mut state = self.state.write().await;
+                    let chat = state.chats.entry(chat_id).or_default();
+                    let entry = chat.subscriptions.entry(channel.clone());
+                    let done = matches!(&entry, Entry::Vacant(..));
+                    entry.or_default();
+                    send_message(
+                        &self.api,
+                        chat_id,
+                        format!(
+                            "You {} to channel <code>{}</code>.",
+                            if done {
+                                "have successfully subscribed"
+                            } else {
+                                "are already subscribed"
+                            },
+                            channel
+                        ),
+                    )
+                    .await?
+                } else {
+                    let state = self.state.read().await;
+                    send_message(
+                        &self.api,
+                        chat_id,
+                        format!(
+                            "Please provide the channel name. For example:\n{}",
+                            state
+                                .channels
+                                .keys()
+                                .map(|id| format!("/subscribe_{id}\n"))
+                                .collect::<String>()
+                        ),
+                    )
+                    .await?
+                }
+            }
+            Command::Unsubscribe { channel } => {
+                if let Some(channel) = channel {
+                    let mut state = self.state.write().await;
+                    let chat = state.chats.entry(chat_id).or_default();
+                    let done = chat.subscriptions.remove(&channel).is_some();
+                    send_message(
+                        &self.api,
+                        chat_id,
+                        format!(
+                            "You {} channel <code>{}</code>.",
+                            if done {
+                                "have successfully unsubscribed from"
+                            } else {
+                                "are not subscribed to"
+                            },
+                            channel
+                        ),
+                    )
+                    .await?
+                } else {
+                    let state = self.state.read().await;
+                    let channels = match state.chats.get(&chat_id) {
+                        Some(chat) => chat.subscriptions.keys().collect::<Vec<_>>(),
+                        None => Vec::new(),
+                    };
+                    send_message(
+                        &self.api,
+                        chat_id,
+                        if channels.is_empty() {
+                            "You have not subscribed to any channel yet.".to_string()
+                        } else {
+                            format!(
+                                "Please provide the channel name. For example:\n{}",
+                                channels
+                                    .into_iter()
+                                    .map(|id| format!("/unsubscribe_{id}\n"))
+                                    .collect::<String>()
+                            )
+                        },
+                    )
+                    .await?
+                }
+            }
+        }
+        Ok(())
+    }
+
+    async fn monitor(self) -> ! {
+        let settings = self.state.read().await.settings.clone();
+
+        loop {
+            sleep(settings.offline_timeout / 2).await;
+
+            let mut messages = Vec::<(ChatId, String)>::new();
+
+            {
+                let state = self.state.read().await;
+                let now = Instant::now();
+                for (channel_id, channel) in state.channels.iter() {
+                    if let Some(last_update) = channel.last_update {
+                        if last_update + settings.offline_timeout > now {
+                            continue;
+                        }
+                    }
+                    for (&chat_id, chat) in state.chats.iter() {
+                        if chat.subscriptions.contains_key(channel_id) {
+                            messages.push((
+                                chat_id,
+                                format!("<b>Alert!</b>\n<code>{}</code> is offline", channel_id),
+                            ));
+                        }
+                    }
+                }
+            }
+
+            for (chat_id, message) in messages {
+                if let Err(err) = send_message(&self.api, chat_id, message).await {
+                    log::error!("Cannot send message: {err}");
+                }
+            }
+        }
     }
 }
 
 impl Recepient for Telegram {
-    type Error = RequestError;
+    type Error = Error;
 
-    async fn update(&mut self, measurements: Measurements) -> Vec<RequestError> {
-        let Self { bot, state } = self;
+    async fn update(&mut self, measurements: Measurements) -> Vec<Error> {
+        let Self { api, state, .. } = self;
         let mut messages = Vec::<(ChatId, String)>::new();
 
         {
@@ -217,7 +431,7 @@ impl Recepient for Telegram {
                             messages.push((
                                 chat_id,
                                 format!(
-                                    "`{}` is online (value: {}).",
+                                    "<code>{}</code> is online (value: {}).",
                                     channel_id,
                                     value_range.display(),
                                 ),
@@ -229,7 +443,7 @@ impl Recepient for Telegram {
                                 messages.push((
                                     chat_id,
                                     format!(
-                                        "**Alert!**\n`{}` value {} is out of normal range {:?}.",
+                                        "<b>Alert!</b>\n<code>{}</code> value {} is out of normal range {:?}.",
                                         channel_id,
                                         value_range.display(),
                                         sub.settings.normal_range,
@@ -246,7 +460,7 @@ impl Recepient for Telegram {
                             messages.push((
                                 chat_id,
                                 format!(
-                                    "`{}` value {} returned to normal range {:?}.",
+                                    "<code>{}</code> value {} returned to normal range {:?}.",
                                     channel_id,
                                     value_range.display(),
                                     sub.settings.normal_range,
@@ -260,43 +474,11 @@ impl Recepient for Telegram {
 
         let mut errors = Vec::new();
         for (chat_id, message) in messages {
-            if let Err(err) = bot.send_message(chat_id, message).await {
+            if let Err(err) = send_message(&api, chat_id, message).await {
                 errors.push(err);
             }
         }
 
         errors
-    }
-}
-
-async fn monitor(bot: Bot, state: SharedState) -> ResponseResult<()> {
-    let settings = state.read().await.settings.clone();
-
-    loop {
-        sleep(settings.offline_timeout / 2).await;
-
-        let mut messages = Vec::<(ChatId, String)>::new();
-
-        {
-            let state = state.read().await;
-            let now = Instant::now();
-            for (channel_id, channel) in state.channels.iter() {
-                if let Some(last_update) = channel.last_update {
-                    if last_update + settings.offline_timeout > now {
-                        continue;
-                    }
-                }
-                for (&chat_id, chat) in state.chats.iter() {
-                    if chat.subscriptions.contains_key(channel_id) {
-                        messages
-                            .push((chat_id, format!("**Alert!**\n`{}` is offline", channel_id)));
-                    }
-                }
-            }
-        }
-
-        for (chat_id, message) in messages {
-            bot.send_message(chat_id, message).await?;
-        }
     }
 }
